a6a0b50125290a0edd2e088bedcba9ac3ddd4214
Forrest Bingham <bingham_5@live.com>
2020-05-18 17:01:12 -0600

    MVP Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,20 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        #pass
+        #8 registers
+        self.reg = [0] * 8
+        #255 storage for ram
+        self.ram = [0] * 256
+        self.pc = 0
+
+    def ram_read(self, MAR):
+
+        return self.ram[MAR]
+    
+    def ram_write(self, MDR, MAR):
+
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -24,7 +37,10 @@ class CPU:
             0b01000111, # PRN R0
             0b00000000,
             0b00000001, # HLT
+            
         ]
+
+
 
         for instruction in program:
             self.ram[address] = instruction
@@ -62,4 +78,57 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        #pass
+        # Look up bits for HLT, LDI, PRN, set running = true
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        running = True
+
+        #while running!:
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8 <-- Save Reg_a in R0
+        #     0b00000000, # reg_a
+        #     0b00001000, # The value 8 == reg_b
+        #     0b01000111, # PRN R0
+        #     0b00000000, 
+        #     0b00000001, # HLT
+        # ]
+
+        while running == True:
+            instruction = self.ram_read(self.pc)
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+
+            if instruction == HLT:
+                print("***** HALT COMMAND INITIATED ******")
+                running = False
+                self.pc += 1
+                sys.exit()
+
+            elif instruction == LDI:
+                print("******** LDI COMMAND INITIATED *******")
+                print(self.reg, "--- self.reg")
+                print(reg_a, "--- reg_a")
+                print(reg_b, "--- reg_b")
+                print(self.reg[reg_a], "--- Before")
+                self.reg[reg_a] = reg_b
+                print(self.reg[reg_a], "--- After")
+                self.pc += 3
+
+                print(self.reg, "--- self.reg")
+
+            elif instruction == PRN:
+                print("******* PRINT COMMAND INITIATED *********")
+                print(self.pc, "--- self.pc")
+                print(self.reg, "--- self.reg")
+                print(reg_a, "--- reg_a")
+                print(self.reg[reg_a])
+                self.pc += 2
+
+            else: 
+                print(f'unknown instruction {instruction} at address {pc}')
+                running = False
+                sys.exit()
+		        
